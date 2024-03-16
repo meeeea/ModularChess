136d941bb685987fdde53e5f4219117c80760c2a
Justin Hutchens <Justinh22007@gmail.com>
2024-03-16 08:56:14 -0500

    added colors and 2 rooks

--- src/boards/Chess.cs
+++ src/boards/Chess.cs
@@ -1,13 +1,19 @@
 class Chess : Board {
     private static Dictionary<Position, Piece> boardInitialization = new Dictionary<Position, Piece>();
-    private static List<Team> teams = new List<Team>() {new Team("W", new StepVector(1, 0)),
-                                                new Team("B", new StepVector(-1, 0))};
+    private static List<Team> teams = new List<Team>() {new Team("W", new StepVector(1, 0), ConsoleColor.Red),
+                                                new Team("B", new StepVector(-1, 0), ConsoleColor.Blue)};
     public Chess() : base(8, 8) {
-        boardInitialization.Add(new Position(0, 0), new Rook(teams[0]));
-        boardInitialization.Add(new Position(7, 0), new Rook(teams[0]));
-
+        AddPieces();
+        
         foreach (KeyValuePair<Position, Piece> entry in boardInitialization) {
             this[entry.Key] = entry.Value;
         }
+    }
+
+    private void AddPieces() {
+        boardInitialization.Add(new Position(0, 0), new Rook(teams[0]));
+        boardInitialization.Add(new Position(7, 0), new Rook(teams[0]));
+        boardInitialization.Add(new Position(0, 7), new Rook(teams[1]));
+        boardInitialization.Add(new Position(7, 7), new Rook(teams[1]));
     }
 }
