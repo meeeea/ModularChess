3e0b7d522ffae247c46460b722e05da2bcd7b4ed
Justin Hutchens <Justinh22007@gmail.com>
2024-03-15 22:30:28 -0500

    modified for children

--- src/pieces/Piece.cs
+++ src/pieces/Piece.cs
@@ -1,13 +1,13 @@
 class Piece {
     // what is display's as. still not sure how this is going to work.
-    private string _displayName = "";
+    protected string _displayName = "";
 
     // move will be described as in Betza's funny notation for fairy chess.
-    private string _move = "";
+    protected string _move = "";
     public string move {get => _move;}
 
     // i intend on adding support for help info to say what a piece does.
-    private string _helpInfo = "";
+    protected string _helpInfo = "";
 
     public Piece(string name = "") {
         _displayName = name;
