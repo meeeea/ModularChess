655a92a885de967d1f9a7b118236628ff1e3a08b
Justin Hutchens <Justinh22007@gmail.com>
2024-03-15 22:30:12 -0500

    removed test calls

--- Main.cs
+++ Main.cs
@@ -2,9 +2,6 @@ class Project {
     public static Board board = new Board(3,5);
 
     public static void Main() {
-        board[0, 0] = new Piece("b");
-        board.ConsoleDisplay();
-        Console.WriteLine(board.width);
-        Console.WriteLine(board.height);
+
     }
 }
