ee162fe63bf0c8d7ee84bb001a355e1e64facf7e
Justin Hutchens <Justinh22007@gmail.com>
2024-03-15 19:46:53 -0500

    added clerity

--- src/pieces/Piece.cs
+++ src/pieces/Piece.cs
@@ -1,5 +1,13 @@
 class Piece {
+    // what is display's as. still not sure how this is going to work.
     private string _displayName = "";
+
+    // move will be described as in Betza's funny notation for fairy chess.
+    private string _move = "";
+    public string move {get => _move;}
+
+    // i intend on adding support for help info to say what a piece does.
+    private string _helpInfo = "";
 
     public Piece(string name = "") {
         _displayName = name;
